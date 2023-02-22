9d52288d1d636978f6f9db7d1761652dccd7712a
Leslie Grace Pelor <lppelor@student.apc.edu.ph>
2023-02-22 22:18:33 +0800

    another update

--- week9/index.php
+++ week9/index.php
@@ -300,12 +300,7 @@
                 $dbname = "testdb";
 
                 // Create connection
-                $conn = new mysqli(
-                    $servername,
-                    $username,
-                    $password,
-                    $dbname
-                );
+                $conn = new mysqli($servername, $username, $password, $dbname);
                 // Check connection
                 if ($conn->connect_error) {
                     die("Connection failed: " . $conn->connect_error);
