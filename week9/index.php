2d43a1d6cf35a5ba056ef00fda6f233f8fa0855a
Leslie Grace Pelor <lppelor@student.apc.edu.ph>
2023-02-23 00:19:18 +0800

    guests

--- week9/index.php
+++ week9/index.php
@@ -320,6 +320,35 @@
 
             ?>
 
+            <?php
+            $servername = "localhost";
+            $username = "username";
+            $password = "password";
+            $dbname = "myDB";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT id, firstname, lastname FROM MyGuests";
+            $result = $conn->query($sql);
+
+            echo "<h1>My Guests</h1>";
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo "<br>";
+                    echo "id: " . $row["id"] . " - " . $row["name"] . " email: " . $row["email"] . " - " . $row["website"] . "<br>";
+                }
+            } else {
+                echo "0 results";
+            }
+            $conn->close();
+
+            ?>
 
         </div>
     </div>
