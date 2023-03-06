39a4b7c8e9632128b8b9d4ed78aad141f222e755
Leslie Grace Pelor <lppelor@student.apc.edu.ph>
2023-03-06 23:45:15 +0800

    update

--- week9/guests.php
+++ week9/guests.php
@@ -24,7 +24,7 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT id, name, email, website, comment, gender, reg_date";
+    $sql = "SELECT name, email, website, comment, gender, reg_date";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
