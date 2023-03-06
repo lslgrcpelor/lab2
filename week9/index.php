9c419760ba7ba2f3404fc456d13f02eb95b55a7b
Leslie Grace Pelor <lppelor@student.apc.edu.ph>
2023-03-06 21:58:58 +0800

    new

--- week9/index.php
+++ week9/index.php
@@ -293,6 +293,7 @@
             // if statement below is for the MySQL insert code to execute only AFTER the submit button is pressed
 
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
                 $servername = "192.168.150.213";
                 $username = "webprogmi212";
                 $password = "b3ntRhino98";
@@ -305,18 +306,23 @@
                     die("Connection failed: " . $conn->connect_error);
                 }
 
-                $sql = "INSERT INTO myguests (name, email, website,comment,gender)
-	                    VALUES ('$name', '$email','$website','$comment','$gender')";
+                $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+                VALUES ('John', 'Doe', 'john@example.com');";
+                $sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+                   VALUES ('Mary', 'Moe', 'mary@example.com');";
+                $sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+                 VALUES ('Julie', 'Dooley', 'julie@example.com')";
 
-                if ($conn->query($sql) === TRUE) {
-                    echo "New record created successfully";
+                if ($conn->multi_query($sql) === TRUE) {
+                    echo "New records created successfully";
                 } else {
                     echo "Error: " . $sql . "<br>" . $conn->error;
                 }
 
-
                 $conn->close();
             }
+
+
 
             ?>
 
